9873f70bf669c8fb8926e6320c3c2df9e6c34aca
Rithvick <sairithvickgofficial@gmail.com>
2025-07-11 01:01:48 +0530

    Solved 2048A

---
+++ CodeForces/Contests/Div 2/2048/2048A.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int tt;
+    cin>>tt;
+    while(tt--){
+        int x;
+        cin>>x;
+        if(x%33 == 0) cout<<"YES\n";
+        else cout<<"NO\n";
+    }
+}
