cb3b63dffd17be10e2ce0f1e2ffa50bb4ffc5f5d
Rithvick <sairithvickgofficial@gmail.com>
2025-08-27 16:14:43 +0530

    Solved Sliding Window Xor

---
+++ CSES/Sliding Window/SlidingWindowXor.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll n,k,x,a,b,c;
+    cin>>n>>k>>x>>a>>b>>c;
+    ll xf,xb;
+    xf =x;
+    xb =x;
+    ll ans, sum = 0;
+    for(int i=0; i<k; i++){
+        sum^=xb;
+        xb = (a*xb + b)%c;
+    }
+    ans = sum;
+    n-=k;
+    while(n>0){
+        sum^=xf;
+        sum^=xb;
+        ans^=sum;
+        xb = (a*xb + b)%c;
+        xf = (a*xf + b)%c;        
+        n--;
+    }
+    cout<<ans;
+}
